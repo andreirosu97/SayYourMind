7570f4e94bfeb5c02e11654e14606b1808b4d56f
Andrei <andreirosu97@gmail.com>
2018-10-07 02:04:46 +0300

    Better criptation for GET on answertome.php

--- api/putRequest.php
+++ api/putRequest.php
@@ -9,9 +9,17 @@
     $date = date("Y/m/d");
     
     $pdo = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_ROOT_USER, DB_ROOT_PASS);
-    $stmt = $pdo->prepare("INSERT INTO ".TBL_REQ." (uid, text, date ) VALUES (?,?,?)");
-    $stmt->execute(array($uid, $text, $date));
-    $stmt = $pdo->prepare("SELECT * FROM ".TBL_REQ." WHERE uid=? ORDER BY reqid DESC");
+    $stmt = $pdo->prepare("SELECT LAST_INSERT_ID() AS NR FROM ".TBL_REQ);
+    $stmt->execute();
+    $nr = $stmt->fetch(PDO::FETCH_ASSOC);
+    $reqid = $nr["NR"];
+    
+    $reqid = hash_hmac("sha512", $reqid, "pizza4life");
+    $data["result"] = $reqid;
+    $stmt = $pdo->prepare("INSERT INTO ".TBL_REQ." (uid, reqid, text, date ) VALUES (?,?,?,?)");
+    $stmt->execute(array($uid, $reqid, $text, $date));
+
+    $stmt = $pdo->prepare("SELECT * FROM ".TBL_REQ." WHERE uid=? ORDER BY nr DESC");
     $stmt->execute(array($uid));
     $res = $stmt->fetch(PDO::FETCH_ASSOC);
 
