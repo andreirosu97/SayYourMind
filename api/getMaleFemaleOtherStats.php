dac364946f7b3aba71a7caecdee2845c7b04fe32
Andrei <andreirosu97@gmail.com>
2018-10-07 22:24:08 +0300

    chart1

--- api/getMaleFemaleOtherStats.php
+++ api/getMaleFemaleOtherStats.php
@@ -7,11 +7,12 @@
 
     
     $pdo = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_ROOT_USER, DB_ROOT_PASS);
-    $stmt = $pdo->prepare("SELECT sex, AVG(rating) AS avg FROM ".TBL_RES."WHERE reqid=?  GROUP BY sex");
-    $stmt->execute(array($reqNumber));
-    $vec = $stmt->fetch_all(PDO::FETCH_ASSOC);
+    $stmt = $pdo->prepare("SELECT sex, AVG(rating) AS avg FROM ".TBL_RES." WHERE reqid=?  GROUP BY sex");
+    $res = $stmt->execute(array($reqNumber));
+    $vec = $stmt->fetchall();
 
-    for ($i=0; $i<3; $i++) 
+
+    for ($i=0; $i<count($vec); $i++) 
         $data[$vec[$i]['sex']]=$vec[$i]['avg'];
 
     echo json_encode($data,JSON_PRETTY_PRINT);
